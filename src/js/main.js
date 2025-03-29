069756c6fc87d5bf20f522c0b49315d6aa6751af
inferno-steel <inferno-steel@mail.ru>
2025-03-29 19:41:52 +0400

    public

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-// test
-document.addEventListener('click', function (e) {
-	let a = e.target;
-	console.log(a);
-	console.log(callWrap.classList.contains('callback__wrap_visible'));
-	console.log(e.target !== callWrap);
-	console.log(e.target !== callWrap && callWrap.classList.contains('callback__wrap_visible'));
-})
